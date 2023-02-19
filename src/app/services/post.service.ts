742539dcd31e9a0c5a6ec6e5329333e16e9c70e8
Neil Haddley <neil@haddley.com>
2023-02-19 17:25:38 -0600

    Added Post Service

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Post } from '../models/post.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  
+  constructor(private http:HttpClient) { }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>('http://localhost:3000/posts');
+  }
+
+  deletePost(id: number): Observable<Post> {
+    return this.http.delete<Post>(`http://localhost:3000/posts/${id}`);
+  }
+
+  addPost(post: Post): Observable<Post> {
+    return this.http.post<Post>('http://localhost:3000/posts', post);
+  }
+
+  updatePost(post: Post): Observable<Post> {
+    return this.http.put<Post>(`http://localhost:3000/posts/${post.id}`, post);
+  }
+
+  getPost(id: number): Observable<Post> {
+    return this.http.get<Post>(`http://localhost:3000/posts/${id}`);
+  }
+  
+}
